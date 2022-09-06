8e655d50621053852f447537b9ee7949a74ab93b
SoloDv <flavioed7ard9@gmail.com>
2022-09-06 15:08:00 -0300

    Files structure ready to start

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b28974ff3f4ecd2a5c186f6e37d9ad24de8d10b6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b28974ff3f4ecd2a5c186f6e37d9ad24de8d10b6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
